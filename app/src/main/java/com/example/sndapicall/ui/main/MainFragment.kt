26764be4ef5040f2fb25cd0ffa523c3a0c47bd97
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-08 20:25:13 +0000

    app-Ordner: 6. MainFragment.kt: MainFragment.kt: viewModel und binding Variablen festlegen, in inCreateView FragmentMainBinding inflaten und root des binding zurückgeben.
    dann in der onViewCreated:
    
    dogAdapter zum Adapter des dogRecyclers machen.
    
    observer für die dogListe aus dem viewModel erstellen, der submitList im dogAdapter aufruft, sobald die
    
    setOnClickListener für den SuchButton, der sobald was neues in die dogs listge geladen wird, submitList im adapter aufruft, der die neue dogs liste in die recyclerview lädt

--- app/src/main/java/com/example/sndapicall/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/sndapicall/ui/main/MainFragment.kt
@@ -6,27 +6,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.sndapicall.R
+import com.example.sndapicall.adapter.DogAdapter
+import com.example.sndapicall.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val dogAdapter = DogAdapter()
+        binding.dogRecycler.adapter = dogAdapter
+
+        viewModel.dogs.observe(viewLifecycleOwner){
+            dogAdapter.submitList(it)
+        }
+
+        viewModel.loading.observe(viewLifecycleOwner){
+            when(it){
+                ApiStatus.LOADING -> {
+                    binding.progressSpinner.visibility = View.VISIBLE
+                }
+                ApiStatus.DONE -> {
+                    binding.progressSpinner.visibility = View.INVISIBLE
+                }
+                ApiStatus.ERROR -> {
+                    binding.progressSpinner.visibility = View.GONE
+                }
+            }
+        }
+
+        binding.searchBtn.setOnClickListener {
+            val term = binding.homeSearchEdit.text.toString()
+            if (term != "") {
+                viewModel.search(term)
+            } else {
+                Toast.makeText(requireContext(), "invalid search", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
 }
